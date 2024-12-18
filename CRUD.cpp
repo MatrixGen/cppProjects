bac216376caf1a3f88bf4dd7a2cb4549dd8ed33e
matrixGen <matrix25102005@gmail.com>
2024-12-18 23:16:06 +0300

    creating fuctions for CRUD

---
+++ CRUD.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class Item
+{
+    string itemName;
+    string ItemId;
+    double itemPrice;
+    string description;
+
+
+
+};
+bool createItem();
+bool deleteItem();
+void readItems();
+bool updateItem();
+
+int main()
+{
+    
+
+}
